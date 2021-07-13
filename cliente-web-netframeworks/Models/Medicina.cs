0b45b7d03e0ced06b7546b2635d0501133f2cb27
Giovani Meza <giovanidav5@outlook.com>
2021-07-12 21:54:41 -0600

    changed folder names

---
+++ cliente-web-netframeworks/Models/Medicina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClienteApi2.Models
+{
+    public class Medicina
+    {
+
+        public int Codigo { get; set; }
+        public string Medicamento { get; set; }
+        public string Dosis { get; set; }
+        public double Precio_unitario { get; set; }
+        public int Cantidad_existencia { get; set; }
+        public string Laboratorio_farmaceutico { get; set; }
+        public string Vencimiento { get; set; }
+        public string Presentacion { get; set; }
+        
+
+    }
+}
